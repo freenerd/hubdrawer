a1b9f4ded6faddee46622c8315498bc1d98c6918
Lars Schneider <larsxschneider@gmail.com>
2013-05-11 13:10:13 +0200

    Parse repo info

--- contentscript.js
+++ contentscript.js
@@ -3,18 +3,43 @@ function log(message) {
 }
 
 
-// Detect a github page. I use this method instead of an URL check to support github enterprise
-// instances.
-function isGithubSite() {
+// Detect a GitHub page. We use this instead of an URL check to support GitHub enterprise instances.
+function isGitHubSite() {
   return window.location.hostname === 'github.com' || 
          $('meta[property="og:site_name"]').attr('content') === 'GitHub';
 }
+
+
+// Returns `true` if the current page contains an GitHub repo.
+function isGitHubRepository() {
+  return $('meta[property="og:type"]').attr('content') === 'githubog:gitrepository';
+}
+
+
+// Performs `func` on the current GitHub repo
+function withLoadedGitHubRepo(func, callback)
+{
+  if (isGitHubRepository()) {
+    var permalink = $('link[rel="permalink"]').attr('href');
+    permalink.replace(
+        /^\/([-A-Za-z0-9_.]+)\/([-A-Za-z0-9_.]+)\/tree\/([a-z0-9]+)(\/.+){0,1}/
+      , function(all, user, repo, hash, path) {
+          func(user, repo, hash, path, callback);
+      })
+    }
+}
+
 
 function injectDrawer() {
   $('#wrapper').append('<div>hallo du</div>');
 }
 
-function fetchTree(owner, repo, sha, callback) {
+
+function fetchTree(owner, repo, sha, path, callback) {
+  alert(owner);
+  alert(repo);
+  alert(sha);
+  alert(path);
   url = "https://api.github.com/repos/" + owner + "/" + repo + "/git/trees/" + sha;
   results = $.getJSON(url, function(data) { callback(data) });
 }
@@ -25,8 +50,8 @@ function displayContent(data) {
 
 chrome.extension.onRequest.addListener(function(request, sender, callback) 
 {
-  if(isGithubSite()) {
+  if(isGitHubSite()) {
     injectDrawer();
-    fetchTree("freenerd", "soundcloud-map", "1acfc31ea1fffd81f34773d783ff8cf526cb4bf1", displayContent);
+    withLoadedGitHubRepo(fetchTree, displayContent);
   }
 });
